6d33b946d0cf5ac9ec21892acb854918b1d7e777
gnanadeepika5 <60079729+gnanadeepika5@users.noreply.github.com>
2020-05-13 14:47:56 -0700

    initial commit

--- src/app/tv-shows-display/tv-shows-display.component.ts
+++ src/app/tv-shows-display/tv-shows-display.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IarrayTvShowsDisplay } from '../itv-shows-display';
+import { TvShowService } from '../tv-show.service';
 
 @Component({
   selector: 'app-tv-shows-display',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tv-shows-display.component.css']
 })
 export class TvShowsDisplayComponent implements OnInit {
-
-  constructor() { }
+currentdata: IarrayTvShowsDisplay;
+  constructor(private tvshowservice: TvShowService) { }
 
   ngOnInit(): void {
+    this.tvshowservice.getShowDetails('girls').subscribe(data => this.currentdata = data)
   }
 
 }
