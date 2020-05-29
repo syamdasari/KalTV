556b6c65b75150e7913686c453020d62f0788787
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2020-05-29 00:58:18 -0700

    getting a schedule by countryCode

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -17,18 +17,21 @@ export class ScheduleComponent implements OnInit {
 
 
   ngOnInit(): void {
-    const today = new Date();
-    const dd = String(today.getDate()).padStart(2, '0');
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    const yyyy = today.getFullYear();
-    this.scheduleService.getScheduleDetails('US', yyyy + '-' + mm + '-' + dd)
-    .subscribe(data => this.localData = data)
-
+    this.getSchedule("US");
   }
 
   onCountrySelect(countryCode){
     console.log(countryCode)
+    this.getSchedule(countryCode)
 
+  }
+  getSchedule(countryCode){
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+    this.scheduleService.getScheduleDetails(countryCode, yyyy + '-' + mm + '-' + dd)
+    .subscribe(data => this.localData = data)
   }
 }
 
